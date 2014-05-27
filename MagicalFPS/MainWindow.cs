a00434b2213e39f4fbd0bae994a194bd41440203
LimeStreem <LimeStreem@gmail.com>
2014-05-27 17:01:45 +0900

    ベースの作成

---
+++ MagicalFPS/MainWindow.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MMF;
+using MMF.Controls.Forms;
+using MMF.Oculus;
+using OculusForMikuMikuFlex;
+
+namespace MagicalFPS
+{
+    public partial class MainWindow :RenderForm
+    {
+        private OculusDisplayRenderer oculusRenderer;
+
+        private OculusDeviceManager oculusDeviceManager;
+
+        public MainWindow(RenderContext context):base(context)
+        {
+            InitializeComponent();
+           
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            oculusDeviceManager=new OculusDeviceManager(RenderContext);
+            oculusRenderer=new OculusDisplayRenderer(RenderContext);
+        }
+
+        public override void Render()
+        {
+            base.Render();
+            oculusRenderer.Render();
+        }
+
+        //protected override void RenderSprite()
+        //{
+
+        //}
+    }
+}
---
+++ MagicalFPS/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MMF;
+
+namespace MagicalFPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initialize RenderContext");
+            RenderContext context=new RenderContext();
+            context.Initialize();
+            MainWindow window=new MainWindow(context);
+            MessagePump.Run(window);
+        }
+    }
+}
